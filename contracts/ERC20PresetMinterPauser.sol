24f52fbfae922d73756a1d3b1f56200cf7b172f9
James Walstonn <james_walstonn@bitbucket.org>
2021-06-27 09:48:33 +0800

    add erc20PresetMinterPauserl.sol for compile of 0.6.12

--- contracts/ERC20PresetMinterPauser.sol
+++ contracts/ERC20PresetMinterPauser.sol
@@ -1,3 +1,4 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity =0.6.12;
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/solc-0.6/contracts/presets/ERC20PresetMinterPauser.sol";
 import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
